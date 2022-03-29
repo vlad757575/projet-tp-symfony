a8b54e746eb6da84d3a53fd7736caa346c6ceeef
vlad757575 <vld.lavrentiev@gmail.com>
2022-03-29 10:42:03 +0200

    2nd commit

---
+++ src/Controller/ReglementInterieurController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReglementInterieurController extends AbstractController
+{
+    /**
+     * @Route("/reglement/interieur", name="app_reglement_interieur")
+     */
+    public function index(): Response
+    {
+        return $this->render('reglement_interieur/index.html.twig', [
+            'controller_name' => 'ReglementInterieurController',
+        ]);
+    }
+}
