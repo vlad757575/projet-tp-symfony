1a978f8dad11c723bdf7f1903b993ccbf69fa5f3
vlad757575 <vld.lavrentiev@gmail.com>
2022-03-29 11:30:43 +0200

    3td commit reglement & home pages

--- src/Controller/ReglementInterieurController.php
+++ src/Controller/ReglementInterieurController.php
@@ -9,11 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ReglementInterieurController extends AbstractController
 {
     /**
-     * @Route("/reglement/interieur", name="app_reglement_interieur")
+     * @Route("/reglement/interieur", name="reglement_interieur")
      */
     public function index(): Response
     {
-        return $this->render('reglement_interieur/index.html.twig', [
+        return $this->render('reglement_interieur/reglement.html.twig', [
             'controller_name' => 'ReglementInterieurController',
         ]);
     }
